56ee3ad75874bf69d68aee596578159c0ad2fb60
ZakariyeYusuf09 <zakariyeyuusuf617@gmail.com>
2024-09-26 21:30:32 +0300

    added reusable color

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -4,6 +4,7 @@ const kScafold = Color(0xff124559);
 const kPrimary = Color(0xffeff6e0);
 const kSecondary = Color(0xffaec3b0);
 const kReusabaleColor = Color(0xff874FCF);
+const kCards = Color(0xFF3C4D53);
 // const kBu = Color(0xff01161e);
 
 const kButtonFbBlue = Color(0xff1C04F5);
