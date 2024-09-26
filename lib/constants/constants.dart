440519cbb4218f1c6f0714478baeddec6347d195
ZakariyeYusuf09 <zakariyeyuusuf617@gmail.com>
2024-09-26 21:41:04 +0300

    removed unnecessary comment

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -5,8 +5,6 @@ const kPrimary = Color(0xffeff6e0);
 const kSecondary = Color(0xffaec3b0);
 const kReusabaleColor = Color(0xff874FCF);
 const kCards = Color(0xFF3C4D53);
-// const kBu = Color(0xff01161e);
-
 const kButtonFbBlue = Color(0xff1C04F5);
 const kButtonFbOrange = Color(0xffDB4A39);
 const kColor = Colors.black;
