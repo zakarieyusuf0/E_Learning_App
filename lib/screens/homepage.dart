3ec5e855d5c2852bad32548b39def414ce3d6f68
ZakariyeYusuf09 <zakariyeyuusuf617@gmail.com>
2024-09-26 16:11:29 +0300

    update

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import '../components/button.dart';
 import '../components/container.dart';
+import '../components/text.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -23,35 +24,37 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: ContainerCard(
-        child: Expanded(
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              Image.asset('assets/Pic1.jpg'),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  textAlign: TextAlign.center,
-                  'The e-Learning app offers a seamless, interactive learning experience with video lessons, quizzes, and assessments.',
-                  style: TextStyle(
-                    fontSize: 17,
-                  ),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Image.asset('assets/Pic1.jpg'),
+            text(
+              textswidget: 'Online Learning Platform',
+              fontSized: 22,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                textAlign: TextAlign.center,
+                'The e-Learning app offers a seamless, interactive learning experience with video lessons, quizzes, and assessments.',
+                style: TextStyle(
+                  fontSize: 17,
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              ButtonCard(
-                text: 'Start Learning home ',
-                Pressed: () {
-                  Navigator.pushNamed(context, '/login');
-                },
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ButtonCard(
+              text: 'Start Learning home ',
+              Pressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+            ),
+          ],
         ),
       ),
     );
